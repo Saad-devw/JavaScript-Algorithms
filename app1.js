4370c2891cfaa63b274ee15460ff4cdd34438fb9
Saad-devw <Saad-Essadiki@users.noreply.github.com>
2020-12-30 13:43:38 +0100

    solve first objective

--- app1.js
+++ app1.js
@@ -1,5 +1,18 @@
+/* first way to solve it */
 function concatfunc(first,second){
   return "hi, my name is : ".concat(first," ",second);
 }
-
 console.log(concatfunc('saad','essadiki'));
+
+/* 2nd way  */
+function concatfunc2(first,second){
+  const myname = "hi, my name is : "+ first + " " + second;
+  return myname;
+}
+console.log(concatfunc2("john","doe"));
+
+/* 3rd way : ES6 Literal templates */
+function concatfunc3(first,second){
+  return `my name is ${first} ${second}`;
+}
+console.log(concatfunc3("alex","mahone"));
