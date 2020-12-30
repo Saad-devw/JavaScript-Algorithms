d095b0f5f8db0caa005f48ec22029988981b708d
Saad-devw <Saad-Essadiki@users.noreply.github.com>
2020-12-30 21:06:13 +0100

    solve objective eight

---
+++ app8.js
@@ -0,0 +1,18 @@
+function upperCaseFirstChar(str){
+
+  let myString = str.split(' ');
+
+  myString = myString.map(function (word) {
+
+    let firstChar = word.charAt(0).toUpperCase();
+    
+    word = firstChar.concat(word.slice(1,word.length));
+    return word;
+  });
+
+  myString = myString.join(' ');
+
+  return myString;
+}
+
+console.log(upperCaseFirstChar("hi my name is saad"));
