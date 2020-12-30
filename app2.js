cbd8ea7fde7f92d996abd03afb1e0a236c99f844
Saad-devw <Saad-Essadiki@users.noreply.github.com>
2020-12-30 16:05:49 +0100

    solve second objective

--- app2.js
+++ app2.js
@@ -1,5 +1,6 @@
 function convertFunc(celsius){
-  var result = celsius * 9/5 + 32; //math logic
+  var result = celsius * 9/5 + 32;
+  //math logic
   return result + 'F°';
 }
 console.log(convertFunc(30));
