1153514114020beb23c4cfdf04d4258a421d78c9
Saad-devw <Saad-Essadiki@users.noreply.github.com>
2020-12-30 13:28:12 +0100

    objective 1 completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Objectives
+  1. create algo that concats your first and last name (app1.js).
+  2. 
---
+++ app1.js
@@ -0,0 +1,5 @@
+function concat(first,sec){
+  return first+' '+sec;
+}
+
+console.log(concat('saad','essadiki'));
