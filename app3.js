02b8854436ad6f72453768c74c1e0949e13a719a
Saad-devw <Saad-Essadiki@users.noreply.github.com>
2020-12-30 15:20:28 +0100

    solve third objective

--- app3.js
+++ app3.js
@@ -1,11 +1,11 @@
 function fizzBuzz(number){
-  if( number >= 3 && number % 3 === 0 && number % 5 !== 0)
+  if(number % 3 === 0 && number % 5 !== 0)
   {
     return 'fizz';
-  }else if(number >= 3 && number % 5 === 0 && number % 3 !== 0)
+  }else if(number % 5 === 0 && number % 3 !== 0)
   {
     return 'buzz';
-  }else if(number >= 3 && number % 3 === 0 && number % 5 === 0)
+  }else if(number % 3 === 0 && number % 5 === 0)
   {
     return 'fizzbuzz';
   }else{
